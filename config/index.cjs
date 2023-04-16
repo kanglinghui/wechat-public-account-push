03db76bf1b565e627841194f865f9100af094590
kanglinghui <38158988+kanglinghui@users.noreply.github.com>
2023-04-16 11:59:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,12 +13,12 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '7d6477c262715c79e4f7df22a1fc2e88',
 
-  province: '上海',
+  province: '上海市',
   city: '上海',
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '我佩♥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqzr35sUy1yNuIR_kZHg-PC4ijfE	',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
