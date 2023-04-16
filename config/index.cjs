8fa5ca12e5636322a33690a93b14ebbcb46305be
kanglinghui <38158988+kanglinghui@users.noreply.github.com>
2023-04-16 11:21:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '7d6477c262715c79e4f7df22a1fc2e88',
+      id: 'oqzr35sUy1yNuIR_kZHg-PC4ijfE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'iJ9zdlZs3wRu9iftitODsEQVckHLtENrpmI6X6KFix8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'iJ9zdlZs3wRu9iftitODsEQVckHLtENrpmI6X6KFix8',
+      id: 'oqzr35sUy1yNuIR_kZHg-PC4ijfE',
     }
   ],
 
